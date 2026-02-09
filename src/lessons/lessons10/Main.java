291cf34e1210d93a67e99ecc440edaac43e0f195
Alexander <a.prepodobniy@yandex.ru>
2026-02-09 18:39:18 +0300

    Выполнил 10 лабораторную

--- src/lessons/lessons10/Main.java
+++ src/lessons/lessons10/Main.java
@@ -1,4 +1,11 @@
 package lessons.lessons10;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        FileUtils.mergeFiles("file1.txt", "file2.txt", "result.txt");
+        FileUtils.replaceSymbols("result.txt");
+        System.out.println("Готово , файлы склеены и обработаны");
+    }
 }
